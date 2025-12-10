544286ccd7aaf03ed86e2d474a1b1e82a1fd9389
hayden.rear <hayden.rear@gmail.com>
2025-12-10 13:48:22 -0500

    updated

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.hayden.spring-app")
+}
+
+group = "com.hayden"
+version = "0.0.1-SNAPSHOT"
+description = "multi-agent-ide"
---
+++ src/main/java/com/hayden/multiagentide/MultiAgentIdeApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.multiagentide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAgentIdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiAgentIdeApplication.class, args);
+    }
+
+}
