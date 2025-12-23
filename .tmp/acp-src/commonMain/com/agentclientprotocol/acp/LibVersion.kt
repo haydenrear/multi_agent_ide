c30228c3579789cc6c8244e66a4f9e733c610493
hayden.rear <hayden.rear@gmail.com>
2025-12-23 18:43:36 -0500

    updated

--- .tmp/acp-src/commonMain/com/agentclientprotocol/acp/LibVersion.kt
+++
@@ -1,3 +0,0 @@
-package com.agentclientprotocol.acp
-
-public const val LIB_VERSION: String = "0.10.1-dev-26"
--- .tmp/acp-src/commonMain/com/agentclientprotocol/common/Event.kt
+++
@@ -1,9 +0,0 @@
-package com.agentclientprotocol.common
-
-import com.agentclientprotocol.model.PromptResponse
-import com.agentclientprotocol.model.SessionUpdate
-
-public sealed class Event {
-    public class SessionUpdateEvent(public val update: SessionUpdate) : Event()
-    public class PromptResponseEvent(public val response: PromptResponse) : Event()
-}
