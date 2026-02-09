bf4adae784317249dc8166197bfa465457424231
hayden.rear <hayden.rear@gmail.com>
2026-02-09 02:10:07 -0500

    tool calls and worktree

--- run-tui.sh
+++ run-tui.sh
@@ -1,3 +1,2 @@
 ./gradlew bootJar
-cd build/libs
-SPRING_PROFILES_ACTIVE=cli java -Dloader.path=/Users/hayde/IdeaProjects/multi_agent_ide_parent/multi_agent_ide_java_parent/multi_agent_ide/src/main/resources -jar multi_agent_ide-0.0.1-SNAPSHOT.jar tui
+SPRING_PROFILES_ACTIVE=cli java -jar build/libs/multi_agent_ide-0.0.1-SNAPSHOT.jar tui
